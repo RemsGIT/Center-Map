755837f3bcda7c1a8188e83399b4de8e21f71156
Remy CASTRO <remycastro27@icloud.com>
2022-10-25 16:48:19 +0200

    Init map and add geojson

---
+++ data/points/upjv.js
@@ -0,0 +1,22 @@
+var upjvPoints = [
+    {
+        name: 'UFR des sciences - bâtiment H',
+        lat: '49.8992061134774',
+        lng: '2.2991091012954716',
+        icon: 'upjvIcon.png'
+    },
+    {
+        name: 'UFR des sciences - bâtiment F',
+        lat: '49.89918019793092',
+        lng: '2.298250794410706',
+        icon: 'upjvIcon.png'
+    },
+    {
+        name: 'UFR des sciences - bâtiment C',
+        lat: '49.89875863642011',
+        lng: '2.299546301364899',
+        icon: 'upjvIcon.png'
+    },
+]
+
+export default upjvPoints
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <map-item />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MapItem from "@/components/Map/MapItem";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'map-item': MapItem
   }
 }
 </script>
 
 <style>
+html,body{width:100%;height: 100%;margin:0}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
 </style>
